a3a3afbb6854448f06873082605a198497e9ca70
Crowdbotics <team@crowdbotics.com>
2023-01-04 12:48:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "clear-ultrasound-38176",
+  url: "https://clear-ultrasound-38176.botics.co",
+  api: "https://clear-ultrasound-38176.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
